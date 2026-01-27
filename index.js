9db0932be86156e1191e083eb720cda1d8210a76
davidmodfyi <david@mod.fyi>
2026-01-27 00:02:38 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Affix!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Affix running on port ${PORT}`);
+});
